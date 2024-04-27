a5927ca510479fc309bdf44d6114d5f8de9c8a88
Khanh Nguyen <khnguy3n@proton.me>
2024-04-27 19:16:36 -0400

    replace hardcode id

--- src/pages/hello.jsx
+++ src/pages/hello.jsx
@@ -1,17 +1,13 @@
-import { getItemsByBox, getBoxById, useQuery } from 'wasp/client/operations'
+import { getItemsByBox, getBoxById, useQuery } from "wasp/client/operations";
 export const HelloPage = (props) => {
-  const { data: box } = useQuery(getBoxById, "1yh4KS9pnirLgpKOl8Z6B");
-  const { data: items } = useQuery(getItemsByBox, "1yh4KS9pnirLgpKOl8Z6B");
+  const { data: box } = useQuery(getBoxById,  props.match.params.name);
+  const { data: items } = useQuery(getItemsByBox, props.match.params.name);
 
   return (
     <div>
-      <h1> Hi {props.match.params.name} mom </h1>
-      <p> should show item data {box && box.name}</p>
-      <div>
-        {items && items.map((box, idx) => (
-          <h1 key={idx}> {box.name} </h1>
-        ))}
-      </div>
+      <h1>Box Name: {box && box.name}</h1>
+      <hr />
+      {items && items.map((box, idx) => <h1 key={idx}> {box.name} </h1>)}
     </div>
-      )
-}
+  );
+};
