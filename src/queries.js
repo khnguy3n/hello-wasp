f3fdf92f65536d51b3409797dfda2b1de6cda814
Khanh Nguyen <khnguy3n@proton.me>
2024-04-27 18:46:01 -0400

    update items query check for table

--- src/queries.js
+++ src/queries.js
@@ -1,18 +1,28 @@
+import { HttpError } from "wasp/server";
+
 export const getUsers = async (args, context) => {
-  return context.entities.User.findMany({
-    orderBy: { id: 'asc' },
-  })
-}
+	return context.entities.User.findMany({
+		orderBy: { id: "asc" },
+	});
+};
 export const getBoxes = async (args, context) => {
-  return context.entities.Box.findMany({
-    orderBy: { id: 'asc' },
-  })
-}
+	return context.entities.Box.findMany({
+		orderBy: { id: "asc" },
+	});
+};
 export const getItemsByBox = async (boxId, context) => {
-  return context.entities.Item.findMany({
-    where: {
-      boxId
-    },
-    orderBy: { id: 'asc' },
-  })
-}
+	if (!context.entities.Item) {
+		throw new HttpError(401);
+	}
+	try {
+		const items = await context.entities.Item.findMany({
+			where: {
+				boxId,
+			},
+			orderBy: { id: "asc" },
+		});
+    return items;
+	} catch (error) {
+		throw new HttpError(404);
+	}
+};
